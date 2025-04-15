31c9e4f0734d406e93bf4540d4fe208a60eab62b
Durga-velu <durgadevi134babu@gmail.com>
2025-04-15 23:25:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
+let strongWords=document.querySelectorAll("strong")
 
+	strongWords.style.color="green"
 
 }
 
 
 function return_normal() {
     //Write your code here
+let strongWords=document.querySelectorAll("strong")
 
+	strongWords.style.color="black"
     
 }
