9ca3c91e9b8f373aed13a1e374d85cb339d7b718
Durga-velu <durgadevi134babu@gmail.com>
2025-04-15 23:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ function highlight() {
     //Write your code here
 let strongWords=document.getElementsByTagName("strong")
 
-	strongWords.textContent.style.color="rgb(0,128,0)"
+	for(let i=0;i<strongWords.length;i++)
+	{
+	strongWords[i].style.color="rgb(0,128,0)"	
+	}
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-let strongWords=document.querySelectorAll("strong")
+let strongWords=document.getElementsByTagName("strong")
 
-	strongWords.style.color="black"
-    
+	
+	for(let i=0;i<strongWords.length;i++)
+	{
+	strongWords[i].style.color="black"	
+	}
+	
 }
